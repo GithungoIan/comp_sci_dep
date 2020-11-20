27759413779d3c1c63f6bb0d574e3842bdb45d51
GithungoIan <githungoian53@gmail.com>
2020-11-20 12:43:23 +0300

    added routes

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const compression = require('compression');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
+const viewRouter = require('./routes/viewRoutes');
+
 const app = req express();
+
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));;
 
@@ -40,10 +47,16 @@ app.use(compression());
 
 
 // ROUTES
+app.use('/', viewRouter);
+app.use('/api/v1/users', userRouter);
+
 
 // TEST Middleware
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 400));
+});
 
 // Error handling
-
+app.use(globalErrorHandler);
 // Export app
 module.exports = app;
